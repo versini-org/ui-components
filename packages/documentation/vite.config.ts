6f4615fe58298b96ee484211de5e9a082122d801
Arno V <versini@gmail.com>
2023-11-05 13:01:33 -0500

    docs: adding documentation package to test

---
+++ packages/documentation/vite.config.ts
@@ -0,0 +1,43 @@
+/// <reference types="vitest" />
+
+import fs from "fs-extra";
+import { defineConfig } from "vite";
+
+const packageJson = fs.readJSONSync("package.json");
+
+const buildTime = new Date()
+	.toLocaleString("en-US", {
+		timeZone: "America/New_York",
+		timeZoneName: "short",
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit",
+		hour: "2-digit",
+		minute: "2-digit",
+	})
+	.replace(/,/g, "");
+
+export default defineConfig({
+	esbuild: {
+		supported: {
+			"top-level-await": true, //browsers can handle top-level-await features
+		},
+	},
+	test: {
+		globals: true,
+		setupFiles: ["./vitest.setup.ts"],
+		environment: "jsdom",
+		coverage: {
+			provider: "v8",
+			lines: 55,
+			functions: 30,
+			branches: 70,
+			statements: 55,
+		},
+	},
+	define: {
+		"import.meta.env.BUILDTIME": JSON.stringify(buildTime),
+		"import.meta.env.BUILDVERSION": JSON.stringify(packageJson.version),
+	},
+	plugins: [],
+});
