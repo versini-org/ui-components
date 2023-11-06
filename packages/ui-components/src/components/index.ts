7648fdfa302a6637034849116ca6a0d507eef734
Arno V <versini@gmail.com>
2023-11-06 12:58:05 -0500

    feat: adding Icons

--- packages/ui-components/src/components/index.ts
+++ packages/ui-components/src/components/index.ts
@@ -2,5 +2,28 @@ import { Button } from "./Button/Button";
 import { ButtonIcon } from "./Button/ButtonIcon";
 import { ButtonLink } from "./Button/ButtonLink";
 import { Footer } from "./Footer/Footer";
+import { IconAssistant } from "./Icons/IconAssistant";
+import { IconClose } from "./Icons/IconClose";
+import { IconCopied } from "./Icons/IconCopied";
+import { IconCopy } from "./Icons/IconCopy";
+import { IconDelete } from "./Icons/IconDelete";
+import { IconDog } from "./Icons/IconDog";
+import { IconRestore } from "./Icons/IconRestore";
+import { IconSettings } from "./Icons/IconSettings";
+import { IconUser } from "./Icons/IconUser";
 
-export { Button, ButtonIcon, ButtonLink, Footer };
+export {
+	Button,
+	ButtonIcon,
+	ButtonLink,
+	Footer,
+	IconAssistant,
+	IconClose,
+	IconCopied,
+	IconCopy,
+	IconDelete,
+	IconDog,
+	IconRestore,
+	IconSettings,
+	IconUser,
+};
---
+++ packages/ui-components/vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, mergeConfig } from "vitest/config";
+
+import viteConfig from "./vite.config";
+
+export default mergeConfig(
+	viteConfig,
+	defineConfig({
+		test: {
+			globals: true,
+			setupFiles: ["./vitest.setup.ts"],
+			environment: "jsdom",
+			coverage: {
+				exclude: ["**/Icons/**"],
+				provider: "v8",
+				lines: 55,
+				functions: 30,
+				branches: 100,
+				statements: 55,
+			},
+		},
+	}),
+);
