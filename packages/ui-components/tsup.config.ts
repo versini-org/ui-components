337ea5fd66d6e390f1b1f7ae61d2e903ce20a688
Arno V <versini@gmail.com>
2023-11-10 15:15:42 -0500

    chore: simplification

--- packages/ui-components/tsup.config.ts
+++ packages/ui-components/tsup.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	format: "esm",
-	entry: {
-		components: "src/components/index.ts",
-		common: "src/common/index.d.ts",
-	},
+	entry: ["src/components/index.ts"],
 	dts: {
 		only: true,
 	},
