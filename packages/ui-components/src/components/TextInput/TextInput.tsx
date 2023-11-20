a771d638e5fd17b0796bbee94b167a244fdcbbb3
Arno V <versini@gmail.com>
2023-11-20 07:39:26 -0500

    wip

--- packages/ui-components/src/components/TextInput/TextInput.tsx
+++ packages/ui-components/src/components/TextInput/TextInput.tsx
@@ -17,12 +17,19 @@ export const TextInput = ({
 	const inputId = useUniqueId({ id, prefix: "av-text-input-" });
 
 	return (
-		<>
+		<span className="av-text-input-wrapper">
+			<input
+				{...extraProps}
+				id={inputId}
+				name={name}
+				type={type}
+				placeholder=" "
+				className="av-text-input block w-full resize-none rounded-md border-none bg-slate-900 px-4 py-3 text-base text-slate-200 placeholder-slate-400 caret-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-300 focus:ring-offset-0 sm:text-base"
+			/>
 			<label htmlFor={inputId} id={labelId}>
 				{label}
 			</label>
-			<input {...extraProps} id={inputId} name={name} type={type} />
 			{helperText && <p>{helperText}</p>}
-		</>
+		</span>
 	);
 };
