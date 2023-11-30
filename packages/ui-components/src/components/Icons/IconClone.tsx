5b09a9dfcaffe72088804eef08a00f6f7d837c3a
Arno V <versini@gmail.com>
2023-11-29 19:39:59 -0500

    wip

--- packages/ui-components/src/components/Icons/IconClone.tsx
+++ packages/ui-components/src/components/Icons/IconClone.tsx
@@ -1,5 +1,9 @@
+/**
+ * This file was automatically generated.
+ * Please do not edit manually.
+ */
+
 import { SvgIcon } from "../private/SvgIcon/SvgIcon";
-import { defaultIconSize } from "./constants";
 import type { IconsProps } from "./IconsTypes";
 
 export const IconClone = ({
@@ -11,7 +15,7 @@ export const IconClone = ({
 	return (
 		<SvgIcon
 			defaultViewBox="0 0 512 512"
-			defaultClassName={defaultIconSize}
+			defaultClassName="h-5 w-5"
 			viewBox={viewBox}
 			className={className}
 			spacing={spacing}
@@ -19,7 +23,7 @@ export const IconClone = ({
 			{...rest}
 		>
 			{/* !Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.*/}
-			<path d="M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z" />
+			<path d="M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z"/>
 		</SvgIcon>
 	);
 };
