c680a1886a499b32c03bcd3a46625d5169199eff
Arno V <versini@gmail.com>
2023-12-02 10:08:50 -0500

    wip

--- packages/ui-components/src/components/TextInput/__tests__/TextInputMask.test.tsx
+++ packages/ui-components/src/components/TextInput/__tests__/TextInputMask.test.tsx
@@ -87,6 +87,223 @@ describe("TextInputMask modifiers", () => {
 		expect(input.className).toContain("toto");
 		expect(input.parentElement?.className).not.toContain("toto");
 	});
+
+	it.skip("should call the `onMaskChange` function with correct parameters on Show/Hide button click", async () => {
+		const user = userEvent.setup();
+		const onMaskChangeHandler = jest.fn();
+		await act(async () => {
+			render(
+				<ThemeProvider>
+					<TextInputMask
+						name="hello"
+						label="hello world"
+						onMaskChange={onMaskChangeHandler}
+					/>
+				</ThemeProvider>,
+			);
+		});
+		const button = screen.getByRole("button");
+		await user.click(button);
+		expect(onMaskChangeHandler).toHaveBeenCalledWith({
+			e: expect.anything(),
+			masked: false,
+		});
+		await user.click(button);
+		expect(onMaskChangeHandler).toHaveBeenCalledWith({
+			e: expect.anything(),
+			masked: true,
+		});
+	});
+	it.skip("should handle the onTextInputMaskBlur blur event when focus moves from the input outside the component", async () => {
+		const user = userEvent.setup();
+		const onTextInputMaskBlurMock = jest.fn();
+		await act(async () => {
+			render(
+				<ThemeProvider>
+					<TextInputMask
+						name="hello"
+						label="hello world"
+						data-testid="txtnptmsk-1"
+						onTextInputMaskBlur={onTextInputMaskBlurMock}
+					/>
+				</ThemeProvider>,
+			);
+		});
+
+		const input = screen.getByTestId("txtnptmsk-1");
+
+		await user.clear(input);
+		await user.tab({ shift: true });
+
+		expect(document.body).toHaveFocus();
+		expect(onTextInputMaskBlurMock).toHaveBeenCalledTimes(1);
+	});
+	it.skip("should handle the onTextInputMaskBlur blur event when focus moves from the Show/Hide button outside the component", async () => {
+		const user = userEvent.setup();
+		const onTextInputMaskBlurMock = jest.fn();
+		await act(async () => {
+			render(
+				<ThemeProvider>
+					<TextInputMask
+						name="hello"
+						label="hello world"
+						data-testid="txtnptmsk-1"
+						onTextInputMaskBlur={onTextInputMaskBlurMock}
+					/>
+				</ThemeProvider>,
+			);
+		});
+
+		const input = screen.getByTestId("txtnptmsk-1");
+		const button = screen.getByRole("button");
+
+		await user.clear(input);
+		await user.tab();
+
+		expect(button).toHaveFocus();
+
+		await user.tab();
+
+		expect(document.body).toHaveFocus();
+		expect(onTextInputMaskBlurMock).toHaveBeenCalledTimes(1);
+	});
+	it.skip("should not handle the onTextInputMaskBlur blur event when focus moves from the input to the Show/Hide button", async () => {
+		const user = userEvent.setup();
+		const onTextInputMaskBlurMock = jest.fn();
+		await act(async () => {
+			render(
+				<ThemeProvider>
+					<TextInputMask
+						name="hello"
+						label="hello world"
+						data-testid="txtnptmsk-1"
+						onTextInputMaskBlur={onTextInputMaskBlurMock}
+					/>
+				</ThemeProvider>,
+			);
+		});
+
+		const input = screen.getByTestId("txtnptmsk-1");
+		const button = screen.getByRole("button");
+
+		await user.clear(input);
+		await user.tab();
+
+		expect(button).toHaveFocus();
+
+		expect(onTextInputMaskBlurMock).not.toHaveBeenCalled();
+	});
+	it.skip("should not handle the onTextInputMaskBlur blur event when focus moves from the Show/Hide button to the input", async () => {
+		const user = userEvent.setup();
+		const onTextInputMaskBlurMock = jest.fn();
+		await act(async () => {
+			render(
+				<ThemeProvider>
+					<TextInputMask
+						name="hello"
+						label="hello world"
+						data-testid="txtnptmsk-1"
+						onTextInputMaskBlur={onTextInputMaskBlurMock}
+					/>
+				</ThemeProvider>,
+			);
+		});
+
+		const input = screen.getByTestId("txtnptmsk-1");
+		const button = screen.getByRole("button");
+
+		await user.clear(input);
+		await user.tab();
+
+		expect(button).toHaveFocus();
+
+		await user.tab({ shift: true });
+
+		expect(input).toHaveFocus();
+		expect(onTextInputMaskBlurMock).not.toHaveBeenCalled();
+	});
+	it.skip("should handle the onBlur blur event when focus moves from the input", async () => {
+		const user = userEvent.setup();
+		const onBlurMock = jest.fn();
+		await act(async () => {
+			render(
+				<ThemeProvider>
+					<TextInputMask
+						name="hello"
+						label="hello world"
+						data-testid="txtnptmsk-1"
+						onBlur={onBlurMock}
+					/>
+				</ThemeProvider>,
+			);
+		});
+
+		const input = screen.getByTestId("txtnptmsk-1");
+		await user.clear(input);
+		await user.tab();
+
+		expect(onBlurMock).toHaveBeenCalled();
+	});
+	it.skip("should call the onValueChange handler when field value is changed", async () => {
+		const user = userEvent.setup();
+		let result = null;
+		const handleSubmit = jest.fn();
+		const handleOnValueChange = (values) => {
+			result = values;
+		};
+
+		await act(async () => {
+			render(
+				<ThemeProvider>
+					<Form onSubmit={handleSubmit}>
+						<TextInputMask
+							name="hello"
+							label="hello world"
+							data-testid="txtnptmsk-1"
+							onValueChange={handleOnValueChange}
+						/>
+					</Form>
+				</ThemeProvider>,
+			);
+		});
+
+		const input = screen.getByTestId("txtnptmsk-1");
+		await user.type(input, "a");
+		const { e, value, name, id, type } = result;
+		expect(value).toBe("a");
+		expect(name).toBe("hello");
+		expect(type).toBe("password");
+		expect(typeof id).toBe("string");
+		expect(typeof e).toBe("object");
+	});
+	it.skip("should receive focus when a form is submitted with an empty required field", async () => {
+		const user = userEvent.setup();
+		const handleSubmit = jest.fn();
+
+		await act(async () => {
+			render(
+				<ThemeProvider>
+					<Form onSubmit={handleSubmit}>
+						<TextInputMask
+							name="hello"
+							label="hello world"
+							data-testid="txtnptmsk-1"
+							required
+						/>
+						<Button type="submit">Submit</Button>
+					</Form>
+				</ThemeProvider>,
+			);
+		});
+
+		const input = screen.getByTestId("txtnptmsk-1");
+		const button = screen.getByText("Submit").parentElement;
+		await user.click(button);
+		expect(handleSubmit.mock.calls).toHaveLength(0);
+		const errorNode = await screen.findAllByText("Enter hello world");
+		expect(errorNode[0]).toHaveAttribute("aria-hidden", "true");
+		expect(input).toHaveFocus();
+	});
 });
 
 describe("TextInputMask methods", () => {
@@ -113,6 +330,30 @@ describe("TextInputMask methods", () => {
 		await user.type(input, "aa");
 
 		expect(spyOnChange).toHaveBeenCalledTimes(2);
+	});
+
+	it("should call the `onMaskChange` function with correct parameters on Show/Hide button click", () => {
+		const onMaskChangeHandler = vi.fn();
+
+		render(
+			<TextInputMask
+				name="hello"
+				label="hello world"
+				onMaskChange={onMaskChangeHandler}
+			/>,
+		);
+
+		const button = screen.getByRole("button");
+		fireEvent.click(button);
+		expect(onMaskChangeHandler).toHaveBeenCalledWith({
+			e: expect.anything(),
+			masked: false,
+		});
+		fireEvent.click(button);
+		expect(onMaskChangeHandler).toHaveBeenCalledWith({
+			e: expect.anything(),
+			masked: true,
+		});
 	});
 });
 
@@ -233,21 +474,17 @@ describe("TextInputMask timer tests", () => {
 		expect(input).toHaveAttribute("type", "password");
 	});
 
-	it.skip("should mask characters after an extended delay when focus/keypress", async () => {
-		jest.useFakeTimers();
-		await act(async () => {
-			render(
-				<ThemeProvider>
-					<TextInputMask
-						data-testid="txtnptmsk-1"
-						name="hello"
-						label="hello world"
-					/>
-				</ThemeProvider>,
-			);
-		});
+	it("should mask characters after an extended delay when focus/keypress", () => {
+		vi.useFakeTimers();
+		render(
+			<TextInputMask
+				data-testid="txtnptmsk-1"
+				name="hello"
+				label="hello world"
+			/>,
+		);
 
-		const buttonLabel = await screen.findByText("Show");
+		const buttonLabel = screen.getByLabelText("Show");
 		expect(buttonLabel).toBeInTheDocument();
 
 		const button = screen.getByRole("button");
@@ -256,43 +493,43 @@ describe("TextInputMask timer tests", () => {
 		fireEvent.click(button);
 		expect(input).toHaveAttribute("type", "text");
 
-		await act(async () => {
-			jest.advanceTimersByTime(10000);
+		act(() => {
+			vi.advanceTimersByTime(10000);
 			fireEvent.change(input, { target: { value: "hello" } });
-			jest.advanceTimersByTime(10000);
+			vi.advanceTimersByTime(10000);
 		});
 		expect(input).toHaveAttribute("type", "text");
 
-		await act(async () => {
-			jest.advanceTimersByTime(20000);
+		act(() => {
+			vi.advanceTimersByTime(20000);
 		});
 		expect(input).toHaveAttribute("type", "password");
 
 		fireEvent.click(button);
 		expect(input).toHaveAttribute("type", "text");
 
-		await act(async () => {
-			jest.advanceTimersByTime(10000);
+		act(() => {
+			vi.advanceTimersByTime(10000);
 			fireEvent.change(input, { target: { value: "" } });
-			jest.advanceTimersByTime(10000);
+			vi.advanceTimersByTime(10000);
 		});
 		expect(input).toHaveAttribute("type", "text");
 
-		await act(async () => {
-			jest.advanceTimersByTime(20000);
+		act(() => {
+			vi.advanceTimersByTime(20000);
 		});
 		expect(input).toHaveAttribute("type", "password");
 
 		fireEvent.click(button);
 		expect(input).toHaveAttribute("type", "text");
-		await act(async () => {
-			jest.advanceTimersByTime(10000);
+		act(() => {
+			vi.advanceTimersByTime(10000);
 
 			// need to focus and blur to reset the internal timeout
 			fireEvent.focus(input);
 			fireEvent.blur(input);
 
-			jest.advanceTimersByTime(10000);
+			vi.advanceTimersByTime(10000);
 		});
 		expect(input).toHaveAttribute("type", "text");
 	});
