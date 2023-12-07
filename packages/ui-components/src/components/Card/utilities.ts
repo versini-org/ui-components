3e345e88517c8cfec63dee07fdef16463e890d8b
Arno V <versini@gmail.com>
2023-12-07 16:37:24 -0500

    Update utilities.ts

--- packages/ui-components/src/components/Card/utilities.ts
+++ packages/ui-components/src/components/Card/utilities.ts
@@ -18,7 +18,7 @@ export const getCardClasses = ({
 	const wrapper = className
 		? className
 		: clsx(CARD_CLASSNAME, "rounded-md text-slate-200", {
-				"border-2 border-b-2 border-slate-900 bg-slate-900 p-4": !noBackground,
+				"border-2 border-slate-900 bg-slate-900 p-4": !noBackground,
 		  });
 	const header = headerClassName
 		? headerClassName
