ed2907bd683e71699fe8d980412dc7e6bf08a063
Arno V <versini@gmail.com>
2023-12-20 16:06:30 -0500

    fix(Icon): missing "decorative" prop from Icons types

--- packages/ui-components/src/components/Icons/IconsTypes.d.ts
+++ packages/ui-components/src/components/Icons/IconsTypes.d.ts
@@ -3,4 +3,5 @@ import { SpacingType } from "../../common";
 export interface IconsProps
 	extends Omit<React.SVGAttributes<SVGElement>, "spacing"> {
 	spacing?: SpacingType;
+	decorative?: boolean;
 }
