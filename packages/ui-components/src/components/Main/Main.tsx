9f55f107b28e217166242b2a55cfd11244826b4d
Arno V <versini@gmail.com>
2024-01-08 13:57:59 -0500

    Main component done

--- packages/ui-components/src/components/Main/Main.tsx
+++ packages/ui-components/src/components/Main/Main.tsx
@@ -1,10 +1,16 @@
 import clsx from "clsx";
 
 import { MAIN_CLASSNAME } from "../../common/constants";
+import { getSpacing } from "../../common/utilities";
 import { MainProps } from "./MainTypes";
 
-export const Main = ({ children, className, raw = false }: MainProps) => {
-	const mainClass = clsx(className, MAIN_CLASSNAME, {
+export const Main = ({
+	children,
+	className,
+	raw = false,
+	spacing,
+}: MainProps) => {
+	const mainClass = clsx(className, MAIN_CLASSNAME, getSpacing(spacing), {
 		"mt-0 flex w-full flex-col p-2 sm:mt-3 md:mx-auto md:max-w-4xl": !raw,
 	});
 
