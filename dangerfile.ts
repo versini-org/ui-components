bd1fa4fe719f05b36698b6dd62ba6dcedb63e1a9
Arno V <versini@gmail.com>
2024-01-15 15:06:53 -0500

    moar

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,23 @@
-import { danger, message } from "danger";
+import { markdown } from "danger";
+import { readFileSync } from "fs";
 
-const modifiedMD = danger.git.modified_files.join("- ");
-message("Changed Files in this PR: \n - " + modifiedMD);
+const res = JSON.parse(String(readFileSync("packages/bundlesize/stats.json")));
+
+const rows = [
+	"| Status | File | Size | Limit |",
+	"| ---- | ---- | ---- | ----- |",
+];
+
+res.forEach((item: any) => {
+	if (item.passed) {
+		rows.push(`✅ | ${item.name} |(${item.size}B)| ${item.sizeLimit}B`);
+	} else {
+		rows.push(`🚫 | ${item.name} |(${item.size}B)| ${item.sizeLimit}B`);
+	}
+});
+
+markdown(
+	`
+## Bundle Size
+` + rows.join("\n"),
+);
