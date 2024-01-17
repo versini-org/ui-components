ec961f4f465c2dc3cdf3bd79fc94be0411231ca8
Arno V <versini@gmail.com>
2024-01-17 12:14:20 -0500

    dangerfile is not easy either

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,7 @@
-import path from "node:path";
+import { basename } from "node:path";
 
-import bytes from "bytes";
 import { fail, markdown } from "danger";
-import fs from "fs-extra";
+import { readJsonSync } from "fs-extra";
 
 type BundlesizeStats = {
 	[key: string]: Array<{
@@ -14,26 +13,86 @@ type BundlesizeStats = {
 	}>;
 };
 
+const map = {
+	b: 1,
+	kb: 1 << 10,
+	mb: 1 << 20,
+	gb: 1 << 30,
+	tb: Math.pow(1024, 4),
+	pb: Math.pow(1024, 5),
+};
+const formatThousandsRegExp = /\B(?=(\d{3})+(?!\d))/g;
+const formatDecimalsRegExp = /(?:\.0*|(\.[^0]+)0+)$/;
+
+function format(value, options) {
+	if (!Number.isFinite(value)) {
+		return null;
+	}
+
+	const mag = Math.abs(value);
+	const thousandsSeparator = (options && options.thousandsSeparator) || "";
+	const unitSeparator = (options && options.unitSeparator) || "";
+	const decimalPlaces =
+		options && options.decimalPlaces !== undefined ? options.decimalPlaces : 2;
+	const fixedDecimals = Boolean(options && options.fixedDecimals);
+	let unit = (options && options.unit) || "";
+
+	if (!unit || !map[unit.toLowerCase()]) {
+		if (mag >= map.pb) {
+			unit = "PB";
+		} else if (mag >= map.tb) {
+			unit = "TB";
+		} else if (mag >= map.gb) {
+			unit = "GB";
+		} else if (mag >= map.mb) {
+			unit = "MB";
+		} else if (mag >= map.kb) {
+			unit = "KB";
+		} else {
+			unit = "B";
+		}
+	}
+
+	const val = value / map[unit.toLowerCase()];
+	let str = val.toFixed(decimalPlaces);
+
+	if (!fixedDecimals) {
+		str = str.replace(formatDecimalsRegExp, "$1");
+	}
+
+	if (thousandsSeparator) {
+		str = str
+			.split(".")
+			.map(function (s, i) {
+				return i === 0
+					? s.replace(formatThousandsRegExp, thousandsSeparator)
+					: s;
+			})
+			.join(".");
+	}
+
+	return str + unitSeparator + unit;
+}
+
 let limitReached = false;
-const res: BundlesizeStats = fs.readJsonSync(
+const res: BundlesizeStats = readJsonSync(
 	"packages/ui-components/tmp/stats.json",
 );
-const { version } = fs.readJsonSync("packages/ui-components/package.json");
-
+const { version } = readJsonSync("packages/ui-components/package.json");
 const rows = [
 	"| Status | File | Size (Gzip) | Limits |",
 	"| ---- | ---- | ---- | ----- |",
 ];
 
 res[version].forEach((item) => {
-	const name = path.basename(item.path);
+	const name = basename(item.path);
 	const passed = item.passed ? "✅" : "🚫";
 	if (!item.passed) {
 		limitReached = true;
 	}
 
 	rows.push(
-		`${passed} | ${name} |${bytes(item.fileSizeGzip, {
+		`${passed} | ${name} |${format(item.fileSizeGzip, {
 			unitSeparator: " ",
 		})}| ${item.limit}`,
 	);
