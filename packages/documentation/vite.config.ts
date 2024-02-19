f9b72f6713cc5c117a19bb89748d6f5b6eb4f14e
Arno V <versini@gmail.com>
2024-02-19 10:54:10 -0500

    docs: improve home page with version and nicer previous and next buttons

---
+++ packages/documentation/vite.config.ts
@@ -0,0 +1,15 @@
+import path from "node:path";
+
+import fs from "fs-extra";
+
+const packageJson = fs.readJSONSync(
+	path.join("..", "ui-components", "package.json"),
+);
+
+export default {
+	define: {
+		"import.meta.env.OWNER": JSON.stringify("gizmette.com"),
+		"import.meta.env.REPOSITORY": JSON.stringify(packageJson.homepage),
+		"import.meta.env.BUILDVERSION": JSON.stringify(packageJson.version),
+	},
+};
