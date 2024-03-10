142b0641451bf73fc2f982f70c18cbff240deaad
Arno V <versini@gmail.com>
2024-03-10 19:16:20 -0400

    fix(ui-icons): removing redundant dependencies and adding them to the plugin (#392)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Refactor**
    - Updated dependencies and configurations across various packages for
    better integration and efficiency.
    - **Tests**
    	- Enhanced test coverage for UI icons in the Tailwind plugin.
    - **Chores**
    - Cleaned up unnecessary dependencies to streamline project
    dependencies.
    - **Bug Fixes**
    	- Fixed assignment of `defaultClassName` property in IconDog component.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- packages/ui-icons/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-import { twPlugin } from "@versini/ui-plugins";
-
-export default twPlugin.merge({
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-});
--- packages/ui-plugins/src/plugins/__tests__/tailwindPlugin.test.ts
+++ packages/ui-plugins/src/plugins/__tests__/tailwindPlugin.test.ts
@@ -4,9 +4,10 @@ import { customContentPath } from "../tailwindcss/tailwindPlugin";
 
 describe("Non-DOM tests", () => {
 	it("should return an array with ui-system and ui-components", () => {
-		expect(customContentPath[0]).toContain("node_modules/@versini/ui-system");
-		expect(customContentPath[1]).toContain(
+		expect(customContentPath[0]).toContain(
 			"node_modules/@versini/ui-components",
 		);
+		expect(customContentPath[1]).toContain("node_modules/@versini/ui-icons");
+		expect(customContentPath[2]).toContain("node_modules/@versini/ui-system");
 	});
 });
