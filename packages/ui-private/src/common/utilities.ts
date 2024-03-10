55e981840fda75678baea89260494f6441acdfb5
Arno V <versini@gmail.com>
2024-03-10 18:50:37 -0400

    fix(ui-private): remove premature optimization and dependencies (#390)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Chores**
    	- Removed unused dependencies to streamline the project.
    - **Refactor**
    - Simplified utility functions for better performance and
    maintainability.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- packages/ui-private/src/common/utilities.ts
+++ packages/ui-private/src/common/utilities.ts
@@ -1,12 +1,3 @@
-import { deepEqual } from "fast-equals";
-import memoize from "micro-memoize";
-
-// import type { SpacingType } from ".";
-
-// export type SpacingProps = {
-// 	spacing?: SpacingType;
-// };
-
 export type SpacingType =
 	| undefined
 	| string
@@ -32,17 +23,6 @@ export type SpacingProps = {
 };
 
 /**
- * Memoize function calls with arguments that are
- * complex objects (hence "deep"). Do not use for
- * standard types.
- * @param {Function} fct - the function to optimize.
- * @returns the optimized function.
- */
-export function memoizeDeep(fct: any) {
-	return memoize(fct, { isEqual: deepEqual });
-}
-
-/**
  * This method returns a string that can be used as a tailwind class relying
  * on margins definitions (m, mt, mb, etc.).
  * Please refer to: https://tailwindcss.com/docs/margin
@@ -54,7 +34,7 @@ export function memoizeDeep(fct: any) {
  * { t: 4, r: 1, b: 3, l: 5} => "mt-4 mr-1 mb-3 ml-5"
  *
  */
-export const getSpacing = memoizeDeep((spacing: SpacingType): string => {
+export const getSpacing = (spacing: SpacingType): string => {
 	let spacingClass = "";
 	/**
 	 * In this case, spacing is a number or a string. For example:
@@ -89,4 +69,4 @@ export const getSpacing = memoizeDeep((spacing: SpacingType): string => {
 	}
 
 	return spacingClass;
-});
+};
