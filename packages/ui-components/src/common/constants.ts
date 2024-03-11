07a842a7e1655c58c3571dd31f52bef7acfa3cdf
Arno V <versini@gmail.com>
2024-03-11 10:53:47 -0400

    Update constants.ts

--- packages/ui-components/src/common/constants.ts
+++ packages/ui-components/src/common/constants.ts
@@ -1,15 +1,10 @@
-export const BUTTON_CLASSNAME = "av-button";
-export const FOOTER_CLASSNAME = "av-footer";
-export const MAIN_CLASSNAME = "av-main";
-export const HEADER_CLASSNAME = "av-header";
-
-export const CARD_CLASSNAME = "av-card";
-
-export const SPINNER_CLASSNAME = "av-spinner";
-
-export const PANEL_CLASSNAME = "av-panel";
-export const MESSAGEBOX_CLASSNAME = "av-messagebox";
-
 export const BUBBLE_CLASSNAME = "av-bubble";
-
+export const BUTTON_CLASSNAME = "av-button";
+export const CARD_CLASSNAME = "av-card";
+export const FOOTER_CLASSNAME = "av-footer";
+export const HEADER_CLASSNAME = "av-header";
+export const MAIN_CLASSNAME = "av-main";
+export const MESSAGEBOX_CLASSNAME = "av-messagebox";
+export const PANEL_CLASSNAME = "av-panel";
 export const PILL_CLASSNAME = "av-pill";
+export const SPINNER_CLASSNAME = "av-spinner";
