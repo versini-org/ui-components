e9b9d5b74ceab21a57199c4098320ba4323980e6
Arno V <versini@gmail.com>
2024-03-16 15:32:55 -0400

    fix(ui-styles): adding as much test coverage as possible (#420)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Tests**
    	- Enhanced testing functionality with code coverage reporting.
    - **New Features**
    	- Improved `TailwindConfig` usability for developers by exporting it.
    - **Refactor**
    - Updated and added tests for better configuration merging in Tailwind
    plugin.
    - **Style**
    - Adjusted internal documentation comments to enhance code
    maintainability.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- packages/ui-styles/src/plugins/__tests__/tailwindPlugin.test.ts
+++ packages/ui-styles/src/plugins/__tests__/tailwindPlugin.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
 
+import { twPlugin } from "..";
 import { customContentPath } from "../tailwindcss/tailwindPlugin";
 
 describe("Non-DOM tests", () => {
@@ -7,7 +8,54 @@ describe("Non-DOM tests", () => {
 		expect(customContentPath[0]).toContain(
 			"node_modules/@versini/ui-components",
 		);
-		expect(customContentPath[1]).toContain("node_modules/@versini/ui-icons");
-		expect(customContentPath[2]).toContain("node_modules/@versini/ui-system");
+		expect(customContentPath[1]).toContain("node_modules/@versini/ui-form");
+		expect(customContentPath[2]).toContain("node_modules/@versini/ui-icons");
+		expect(customContentPath[3]).toContain("node_modules/@versini/ui-system");
+	});
+
+	it("should return a default configuration", () => {
+		const config: any = {};
+
+		const mergedConfig = twPlugin.merge(config);
+		const content = (mergedConfig.content as string[]).join(" ");
+
+		expect(content).not.toContain("./src/*.{js,ts,jsx,tsx}");
+		expect(content).toContain("node_modules/@versini/ui-components");
+		expect(content).toContain("node_modules/@versini/ui-form");
+		expect(content).toContain("node_modules/@versini/ui-icons");
+		expect(content).toContain("node_modules/@versini/ui-system");
+
+		expect(mergedConfig.safelist).toContain("mt-0");
+		expect(mergedConfig.safelist).toContain("mt-52");
+		expect(mergedConfig.safelist).not.toContain("mt-53");
+		expect(mergedConfig.safelist).not.toContain("safelist");
+
+		expect(mergedConfig.plugins).not.toContain("plugins");
+		expect(mergedConfig.plugins).toHaveLength(2);
+	});
+
+	it("should return a merged configuration", () => {
+		const config: any = {
+			content: ["./src/*.{js,ts,jsx,tsx}"],
+			safelist: ["safelist"],
+			plugins: ["plugins"],
+		};
+
+		const mergedConfig = twPlugin.merge(config);
+		const content = (mergedConfig.content as string[]).join(" ");
+
+		expect(content).toContain("./src/*.{js,ts,jsx,tsx}");
+		expect(content).toContain("node_modules/@versini/ui-components");
+		expect(content).toContain("node_modules/@versini/ui-form");
+		expect(content).toContain("node_modules/@versini/ui-icons");
+		expect(content).toContain("node_modules/@versini/ui-system");
+
+		expect(mergedConfig.safelist).toContain("mt-0");
+		expect(mergedConfig.safelist).toContain("mt-52");
+		expect(mergedConfig.safelist).not.toContain("mt-53");
+		expect(mergedConfig.safelist).toContain("safelist");
+
+		expect(mergedConfig.plugins).toContain("plugins");
+		expect(mergedConfig.plugins).toHaveLength(3);
 	});
 });
