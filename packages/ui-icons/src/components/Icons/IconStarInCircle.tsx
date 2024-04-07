78b876d582e43e760213b12a3865b373b65352bd
Arno V <versini@gmail.com>
2024-04-07 19:37:48 -0400

    feat(ui-icons): adding IconStarInCircle

---
+++ packages/ui-icons/src/components/Icons/IconStarInCircle.tsx
@@ -0,0 +1,49 @@
+/**
+ * This file was automatically generated.
+ * Please do not edit manually.
+ *
+ * To update this file, run `yarn build:icons`.
+ *
+ * Original name: circle-star.svg
+ *
+ * !Font Awesome Pro 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2024 Fonticons, Inc.
+ *
+ */
+
+import { SvgIcon } from "@versini/ui-private";
+
+import type { IconsProps } from "./IconsTypes";
+
+export const IconStarInCircle = ({
+	className,
+	viewBox,
+	spacing,
+	title,
+
+	monotone,
+	...rest
+}: IconsProps) => {
+	/* v8 ignore next 1 */
+	const opacity = monotone ? "1" : "0.4";
+	return (
+		<SvgIcon
+			defaultViewBox="0 0 512 512"
+			defaultClassName="h-5 w-5"
+			viewBox={viewBox}
+			className={className}
+			spacing={spacing}
+			title={title || "Star in Circle"}
+			{...rest}
+		>
+			<path
+				className="fa-secondary"
+				opacity={opacity}
+				d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM256 104c-6.1 0-11.7 3.5-14.3 8.9l-36.2 73.4-81 11.8c-6 .9-11 5.1-12.9 10.9s-.3 12.2 4 16.4l58.6 57.2-13.8 80.7c-1 6 1.4 12.1 6.4 15.6s11.5 4.1 16.8 1.2L256 342.1l72.5 38.1c5.4 2.8 11.9 2.4 16.9-1.2s7.4-9.6 6.4-15.6l-13.8-80.7 58.6-57.2c4.4-4.3 5.9-10.6 4-16.4s-6.9-10-12.9-10.9l-81.1-11.8-36.2-73.4c-2.7-5.5-8.3-8.9-14.3-8.9z"
+			/>
+			<path
+				className="fa-primary"
+				d="M270.3 112.9c-2.7-5.5-8.3-8.9-14.3-8.9s-11.7 3.5-14.3 8.9l-36.2 73.4-81.1 11.8c-6 .9-11 5.1-12.9 10.9s-.3 12.2 4 16.4l58.6 57.2-13.8 80.7c-1 6 1.4 12.1 6.4 15.6s11.5 4.1 16.9 1.2L256 342.1l72.5 38.1c5.4 2.8 11.9 2.4 16.9-1.2s7.4-9.6 6.4-15.6l-13.8-80.7 58.6-57.2c4.4-4.3 5.9-10.6 4-16.4s-6.9-10-12.9-10.9l-81-11.8-36.2-73.4z"
+			/>
+		</SvgIcon>
+	);
+};
