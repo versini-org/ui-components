830a492b543bc9d3021ade0f1e34c0b4ea1f0ea4
Arno V <versini@gmail.com>
2024-04-18 11:11:08 -0400

    fix(ui-styles): TextArea helper text is misaligned

--- packages/ui-styles/src/plugins/tailwindcss/components/textArea.ts
+++ packages/ui-styles/src/plugins/tailwindcss/components/textArea.ts
@@ -1,18 +1,19 @@
 export default {
-	[`.av-text-area-wrapper label[aria-hidden="true"]`]: {
+	'.av-text-area-wrapper label[aria-hidden="true"]': {
 		/* move the label inline */
 		transform: "translate(18px, 0) scale(1)",
 		transformOrigin: "top left",
 		transition: "var(--av-text-area-wrapper-transition, all 0.2s ease-out)",
 	},
-	'.av-text-area:focus + label[aria-hidden="true"],\n\t.av-text-area:not(:placeholder-shown) + label[aria-hidden="true"]':
-		{
-			transform:
-				"translate(18px, var(--av-text-area-label, -25px)) scale(0.75)",
-		},
+	/* move the label above the field (on focus) */
+	[`.av-text-area:focus + label[aria-hidden="true"],
+	.av-text-area:not(:placeholder-shown) + label[aria-hidden="true"]`]: {
+		transform: "translate(18px, var(--av-text-area-label, -25px)) scale(0.75)",
+	},
+	/* move the helper text below the field */
 	".av-text-area-helper-text": {
 		transform:
-			"translate(18px, var(--av-text-area-helper-text, 32px))\n\t\t\tscale(0.75)",
+			"translate(12px, var(--av-text-area-helper-text, 32px)) scale(0.75)",
 		transformOrigin: "top left",
 	},
 	".av-text-area__control--right": {
