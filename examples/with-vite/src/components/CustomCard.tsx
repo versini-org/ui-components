840113be1cde02e5aadf2a70fed3556dea1826d2
Arno V <versini@gmail.com>
2024-04-26 14:11:01 -0400

    perf: refactor Panel to lazy load heavy imports (#535)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new nested `Panel` component within the `CustomCard` for
    enhanced profile interaction.
    - Enhanced user interaction with updated `MenuItem` behavior in the
    `CustomCard`.
      
    - **Refactor**
    - Implemented lazy loading for modal components in the `Panel` to
    optimize performance and loading times.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- examples/with-vite/src/components/CustomCard.tsx
+++ examples/with-vite/src/components/CustomCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Menu, MenuItem } from "@versini/ui-components";
+import { Card, Menu, MenuItem, Panel } from "@versini/ui-components";
 import {
 	IconChart,
 	IconHistory,
@@ -7,28 +7,44 @@ import {
 	IconSettings,
 } from "@versini/ui-icons";
 import { Flexgrid, FlexgridItem } from "@versini/ui-system";
+import { useState } from "react";
 import { CommonTemplate } from "./CommonTemplate";
 
 export const CustomCard = () => {
+	const [open, setOpen] = useState(false);
+	const onOpenChange = () => {
+		setOpen(!open);
+	};
 	return (
-		<Card
-			header={
-				<Flexgrid alignHorizontal="space-between">
-					<FlexgridItem>
-						<h2 className="m-0 mb-1">Kitchen Sink</h2>
-					</FlexgridItem>
-					<FlexgridItem>
-						<Menu icon={<IconSettings />}>
-							<MenuItem label="Profile" icon={<IconProfile />} />
-							<MenuItem label="Statistics" icon={<IconChart />} />
-							<MenuItem label="History" icon={<IconHistory />} />
-							<MenuItem label="About" icon={<IconInfo />} />
-						</Menu>
-					</FlexgridItem>
-				</Flexgrid>
-			}
-		>
-			<CommonTemplate />
-		</Card>
+		<>
+			<Panel title="Profile" open={open} onOpenChange={onOpenChange}>
+				Hello Profile
+			</Panel>
+			<Card
+				header={
+					<Flexgrid alignHorizontal="space-between">
+						<FlexgridItem>
+							<h2 className="m-0 mb-1">Kitchen Sink</h2>
+						</FlexgridItem>
+						<FlexgridItem>
+							<Menu icon={<IconSettings />}>
+								<MenuItem
+									label="Profile"
+									icon={<IconProfile />}
+									onClick={() => {
+										setOpen(true);
+									}}
+								/>
+								<MenuItem label="Statistics" icon={<IconChart />} />
+								<MenuItem label="History" icon={<IconHistory />} />
+								<MenuItem label="About" icon={<IconInfo />} />
+							</Menu>
+						</FlexgridItem>
+					</Flexgrid>
+				}
+			>
+				<CommonTemplate />
+			</Card>
+		</>
 	);
 };
