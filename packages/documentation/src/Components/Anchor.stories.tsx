4230dd549cbf0851a738cb3adb47561c884f00b9
Arno V <versini@gmail.com>
2024-07-18 08:25:57 -0400

    Update Anchor.stories.tsx

--- packages/documentation/src/Components/Anchor.stories.tsx
+++ packages/documentation/src/Components/Anchor.stories.tsx
@@ -1,12 +1,5 @@
 import type { Story } from "@ladle/react";
 import { Anchor } from "@versini/ui-components";
-import {
-	getFingerprintData,
-	getFingerprintHash,
-} from "@versini/ui-fingerprint";
-
-const fpData = await getFingerprintData();
-const fpHash = await getFingerprintHash();
 
 export default {
 	title: "Components/Anchor",
@@ -40,18 +33,14 @@ export default {
 	},
 };
 
-export const Basic: Story<any> = (args) => {
-	return (
-		<div className="flex flex-wrap gap-2">
-			<Anchor {...args}>Anchor as a button</Anchor>
-			<Anchor {...args}>Anchor as a button lorem ipsum</Anchor>
-			<Anchor {...args}>Anchor as a button lorem ipsum dolor</Anchor>
-			<Anchor {...args}>Anchor as a button lorem ipsum dolor sit amet</Anchor>
-			<pre>{JSON.stringify(fpHash, null, 2)}</pre>
-			<pre>{JSON.stringify(fpData, null, 2)}</pre>
-		</div>
-	);
-};
+export const Basic: Story<any> = (args) => (
+	<div className="flex flex-wrap gap-2">
+		<Anchor {...args}>Anchor as a button</Anchor>
+		<Anchor {...args}>Anchor as a button lorem ipsum</Anchor>
+		<Anchor {...args}>Anchor as a button lorem ipsum dolor</Anchor>
+		<Anchor {...args}>Anchor as a button lorem ipsum dolor sit amet</Anchor>
+	</div>
+);
 
 export const NewWindow: Story<any> = (args) => (
 	<div className="flex flex-wrap gap-2">
