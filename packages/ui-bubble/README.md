851c04bcbc107493fccea9c22ed5f05c831fe197
Arno V <versini@gmail.com>
2024-09-16 17:44:54 -0400

    feat(Bubble): extracting Bubble as a standalone package

---
+++ packages/ui-bubble/README.md
@@ -0,0 +1,3 @@
+# @versini/ui-anchor
+
+A simple anchor component for React.
---
+++ packages/ui-bubble/src/components/Bubble/BubbleTypes.d.ts
@@ -0,0 +1,50 @@
+import type { SpacingProps } from "@versini/ui-private/dist/utilities";
+
+export type BubbleProps = {
+	/**
+	 * The children to render.
+	 */
+	children: React.ReactNode;
+	/**
+	 * CSS class(es) to add to the main component wrapper.
+	 */
+	className?: string;
+	/**
+	 * Whether or not to show a "copy/paste" icon next to the Bubble.
+	 */
+	copyToClipboard?: boolean | string | ((text: any) => void);
+	/**
+	 * The type of focus for the Copy Button. This will change the color
+	 * of the focus ring around the Button.
+	 */
+	copyToClipboardFocusMode?: "dark" | "light" | "system" | "alt-system";
+	/**
+	 * The mode of Copy Button. This will change the color of the Button.
+	 */
+	copyToClipboardMode?: "dark" | "light" | "system" | "alt-system";
+	/**
+	 * Object depicting the extra rows for the Bubble.
+	 * @example
+	 * ```tsx
+	 * <Bubble
+	 * 	footer={{
+	 * 		"Sent": "12:00 PM",
+	 * 		"Delivered": "12:01 PM",
+	 * 		"Read": "12:02 PM",
+	 * 	}}
+	 * >Hello World</Bubble>
+	 * ```
+	 */
+	footer?: {
+		[key: string]: string | number | undefined | null;
+	};
+	/**
+	 * The type of Bubble. This will change the color of the Bubble
+	 * as well as the chevron location.
+	 */
+	kind?: "left" | "right";
+	/**
+	 * Same as "footer" but accepts raw JSX.
+	 */
+	rawFooter?: React.ReactNode;
+} & SpacingProps;
