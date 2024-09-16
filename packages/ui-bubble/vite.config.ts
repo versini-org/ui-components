0295ea67fe6307cf763c1a343a42eba865e974e7
Arno V <versini@gmail.com>
2024-09-16 19:13:41 -0400

    fix(Bubble): invalid name for analytics

--- packages/ui-bubble/vite.config.ts
+++ packages/ui-bubble/vite.config.ts
@@ -1,6 +1,6 @@
 import { commonViteConfigForComponent } from "../../configuration/vite.common";
 
 export default commonViteConfigForComponent({
-	globalComponentName: "__VERSINI_UI_ANCHOR__",
-	globalLibraryName: "UIAnchor",
+	globalComponentName: "__VERSINI_UI_BUBBLE__",
+	globalLibraryName: "UIBubble",
 });
