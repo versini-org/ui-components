91c6c857e38f8368c509a04e63912a35e75c2053
Arno V <versini@gmail.com>
2024-09-17 12:54:57 -0400

    feat: simplification of the global name for individual components (#652)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    	- Introduced a new interface for enhanced configuration flexibility.
    	- Improved logic for generating package and library names.
    
    - **Bug Fixes**
    - Removed specific global component and library names to prevent
    potential registration issues.
    
    - **Refactor**
    - Simplified configuration exports across multiple components for a more
    generic setup.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- packages/ui-button/vite.config.ts
+++ packages/ui-button/vite.config.ts
@@ -1,6 +1,2 @@
 import { commonViteConfigForComponent } from "../../configuration/vite.common";
-
-export default commonViteConfigForComponent({
-	globalComponentName: "__VERSINI_UI_BUTTON__",
-	globalLibraryName: "UIButton",
-});
+export default commonViteConfigForComponent();
