810d80817450d7d24477a70017f7c9dbc3ae1eb5
Arno V <versini@gmail.com>
2024-09-18 11:22:04 -0400

    feat(TextArea): extracting TextArea as a standalone package

--- packages/ui-form/src/components/index.ts
+++ packages/ui-form/src/components/index.ts
@@ -1,6 +1,5 @@
+import { TextArea } from "@versini/ui-textarea";
 import { TextInput, TextInputMask } from "@versini/ui-textinput";
 import { Toggle } from "@versini/ui-toggle";
-
-import { TextArea } from "./TextArea/TextArea";
 
 export { TextArea, TextInput, TextInputMask, Toggle };
