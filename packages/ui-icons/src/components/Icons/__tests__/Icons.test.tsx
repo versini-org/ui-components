2ab59069547c8126afab21f2031678bb6aa702b2
Arno V <versini@gmail.com>
2024-10-02 09:40:58 -0400

    feat(ui-icons): adding IconMessages and IconBookmark

--- packages/ui-icons/src/components/Icons/__tests__/Icons.test.tsx
+++ packages/ui-icons/src/components/Icons/__tests__/Icons.test.tsx
@@ -1,40 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { SpacingProps } from "@versini/ui-private/dist/utilities";
 
-import {
-	IconAdd,
-	IconBack,
-	IconBrightness,
-	IconChart,
-	IconClose,
-	IconCopied,
-	IconCopy,
-	IconDelete,
-	IconDog,
-	IconDown,
-	IconEdit,
-	IconGitHub,
-	IconHide,
-	IconHistory,
-	IconInfo,
-	IconKey,
-	IconLocked,
-	IconNext,
-	IconPasskey,
-	IconPrevious,
-	IconProfile,
-	IconRefresh,
-	IconRestore,
-	IconSearch,
-	IconSettings,
-	IconShow,
-	IconSort,
-	IconSortDown,
-	IconSortUp,
-	IconStarInCircle,
-	IconUp,
-	IconUser,
-} from "../..";
+import * as AllIcons from "../..";
 
 const defaultIconSize = "size-5";
 
@@ -50,7 +17,7 @@ const renderExpected = async ({
 	fill?: string;
 	viewBox?: string;
 } & SpacingProps) => {
-	const svg = await screen.findByTestId(dataTestId);
+	const svg = screen.getByTestId(dataTestId);
 	expect(svg.getAttribute("fill")).toBe(fill ? fill : "currentColor");
 	expect(svg.getAttribute("viewBox")).toContain(viewBox ? viewBox : "0 0 ");
 	expect(svg.getAttribute("class")).toContain(
@@ -67,283 +34,6 @@ const renderExpected = async ({
 	}
 };
 
-describe("Generic Icons prop tests", () => {
-	it.each`
-		fill         | viewBox          | className    | spacing       | description
-		${"red"}     | ${"0 0 666 666"} | ${"toto"}    | ${undefined}  | ${"with fill, viewBox and className"}
-		${undefined} | ${undefined}     | ${undefined} | ${undefined}  | ${"with default fill, default viewBox and default className"}
-		${undefined} | ${undefined}     | ${undefined} | ${"666"}      | ${"with string spacing"}
-		${undefined} | ${undefined}     | ${undefined} | ${666}        | ${"with number spacing"}
-		${undefined} | ${undefined}     | ${undefined} | ${{ t: 666 }} | ${"with object spacing"}
-	`(
-		"should render Icons $description",
-		async ({ fill, viewBox, className, spacing }) => {
-			render(
-				<>
-					<IconChart
-						data-testid="icon-chart"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconHistory
-						data-testid="icon-history"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconInfo
-						data-testid="icon-info"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconProfile
-						data-testid="icon-profile"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconClose
-						data-testid="icon-close"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconCopied
-						data-testid="icon-copied"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconCopy
-						data-testid="icon-copy"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconDelete
-						data-testid="icon-delete"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconEdit
-						data-testid="icon-edit"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconRestore
-						data-testid="icon-restore"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconSettings
-						data-testid="icon-settings"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconUser
-						data-testid="icon-user"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconHide
-						data-testid="icon-hide"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconShow
-						data-testid="icon-show"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconBack
-						data-testid="icon-back"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconNext
-						data-testid="icon-next"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconPrevious
-						data-testid="icon-previous"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconGitHub
-						data-testid="icon-github"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconBrightness
-						data-testid="icon-brightness"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconDown
-						data-testid="icon-down"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconUp
-						data-testid="icon-up"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconAdd
-						data-testid="icon-add"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconStarInCircle
-						data-testid="icon-star-in-circle"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconSort
-						data-testid="icon-sort"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconSortDown
-						data-testid="icon-sort-down"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconSortUp
-						data-testid="icon-sort-up"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconRefresh
-						data-testid="icon-refresh"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconLocked
-						data-testid="icon-locked"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconKey
-						data-testid="icon-key"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconPasskey
-						data-testid="icon-passkey"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-					<IconSearch
-						data-testid="icon-search"
-						className={className}
-						fill={fill}
-						viewBox={viewBox}
-						spacing={spacing}
-					/>
-				</>,
-			);
-			[
-				"icon-back",
-				"icon-chart",
-				"icon-history",
-				"icon-info",
-				"icon-profile",
-				"icon-close",
-				"icon-copied",
-				"icon-copy",
-				"icon-delete",
-				"icon-edit",
-				"icon-restore",
-				"icon-settings",
-				"icon-user",
-				"icon-hide",
-				"icon-show",
-				"icon-next",
-				"icon-previous",
-				"icon-github",
-				"icon-brightness",
-				"icon-down",
-				"icon-up",
-				"icon-add",
-				"icon-star-in-circle",
-				"icon-sort",
-				"icon-sort-down",
-				"icon-sort-up",
-				"icon-refresh",
-				"icon-locked",
-				"icon-key",
-				"icon-passkey",
-				"icon-search",
-			].forEach(async (dataTestId) => {
-				await renderExpected({
-					dataTestId,
-					fill,
-					viewBox,
-					className,
-					spacing,
-				});
-			});
-		},
-	);
-});
-
 describe("Custom IconDog prop tests", () => {
 	it.each`
 		fill         | viewBox          | className    | spacing       | description
@@ -356,7 +46,7 @@ describe("Custom IconDog prop tests", () => {
 		"should render IconDog $description",
 		async ({ fill, viewBox, className, spacing }) => {
 			render(
-				<IconDog
+				<AllIcons.IconDog
 					data-testid="icon-dog"
 					className={className}
 					fill={fill}
@@ -380,6 +70,50 @@ describe("Custom IconDog prop tests", () => {
 			if (typeof spacing === "object") {
 				expect(svg.getAttribute("class")).toContain(`mt-${spacing.t}`);
 			}
+		},
+	);
+});
+
+describe("Generic Icons prop tests", () => {
+	it.each`
+		fill         | viewBox          | className    | spacing       | description
+		${"red"}     | ${"0 0 666 666"} | ${"toto"}    | ${undefined}  | ${"with fill, viewBox and className"}
+		${undefined} | ${undefined}     | ${undefined} | ${undefined}  | ${"with default fill, default viewBox and default className"}
+		${undefined} | ${undefined}     | ${undefined} | ${"666"}      | ${"with string spacing"}
+		${undefined} | ${undefined}     | ${undefined} | ${666}        | ${"with number spacing"}
+		${undefined} | ${undefined}     | ${undefined} | ${{ t: 666 }} | ${"with object spacing"}
+	`(
+		"should render Icons $description",
+		async ({ fill, viewBox, className, spacing }) => {
+			render(
+				<div>
+					{Object.keys(AllIcons).map((icon) => {
+						const Icon = AllIcons[icon as keyof typeof AllIcons];
+						return icon !== "IconDog" ? (
+							<Icon
+								key={icon}
+								data-testid={icon}
+								className={className}
+								fill={fill}
+								viewBox={viewBox}
+								spacing={spacing}
+							/>
+						) : null;
+					})}
+				</div>,
+			);
+
+			Object.keys(AllIcons).map(async (icon) => {
+				if (icon !== "IconDog") {
+					await renderExpected({
+						dataTestId: icon,
+						fill,
+						viewBox,
+						className,
+						spacing,
+					});
+				}
+			});
 		},
 	);
 });
