f2f4f93106a9606fa00c1085117564e6d6a76942
Arno V <versini@gmail.com>
2024-12-16 10:30:29 -0500

    Update preview.js

--- examples/with-vite/.storybook/preview.js
+++ examples/with-vite/.storybook/preview.js
@@ -5,6 +5,8 @@ import "@versini/ui-button/dist/style.css";
 const preview = {
 	parameters: {
 		controls: {
+			// show description and default values in the individual stories (basic, etc.)
+			expanded: true,
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i,
