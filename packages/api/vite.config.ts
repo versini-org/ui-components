50f2af50878e6361eee6bb9643d3cfb3f3b810ab
Arno V <versini@gmail.com>
2024-12-17 11:27:45 -0500

    docs: fix build error for top level await in API docs

--- packages/api/vite.config.ts
+++ packages/api/vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	esbuild: {
+		supported: {
+			"top-level-await": true, //browsers can handle top-level-await features
+		},
+	},
 	plugins: [react()],
 });
