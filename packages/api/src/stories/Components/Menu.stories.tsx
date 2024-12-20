5bdbb80438fb605401e53ab142da2ffa9b4c001c
Arno V <versini@gmail.com>
2024-12-19 19:42:03 -0500

    docs: adding Menu to API docs

---
+++ packages/api/src/stories/Components/Menu.stories.tsx
@@ -0,0 +1,90 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import "@versini/ui-menu/dist/style.css";
+import { Button, ButtonIcon } from "@versini/ui-button";
+import {
+	IconChart,
+	IconHistory,
+	IconInfo,
+	IconProfile,
+	IconSettings,
+} from "@versini/ui-icons";
+
+import { Menu, MenuItem } from "../../../../ui-menu/src/components";
+import { commonControlsSetup } from "../../helpers/constants";
+
+type Story = StoryObj<typeof Menu>;
+
+const meta: Meta<typeof Menu> = {
+	parameters: {
+		layout: "centered",
+		docs: { ...commonControlsSetup.parameters },
+	},
+	title: "Components/Menu",
+
+	component: Menu,
+	args: {
+		onClick: fn(),
+		label: "Open menu",
+		defaultPlacement: "bottom-start",
+		mode: "system",
+		focusMode: "system",
+	},
+};
+
+export default meta;
+
+export const Basic: Story = {
+	...commonControlsSetup,
+	render: (args) => (
+		<div className="flex h-56 flex-wrap p-11">
+			<Button size="small" noBorder spacing={{ r: 2 }}>
+				Button
+			</Button>
+			<Menu
+				{...args}
+				trigger={
+					<ButtonIcon>
+						<IconSettings />
+					</ButtonIcon>
+				}
+				spacing={{ r: 2 }}
+			>
+				<MenuItem label="Profile" />
+				<MenuItem label="Statistics" />
+				<MenuItem label="History" />
+				<MenuItem label="About" />
+			</Menu>
+			<Button size="small" noBorder>
+				Button
+			</Button>
+		</div>
+	),
+};
+
+export const WithIcons: Story = {
+	...commonControlsSetup,
+	render: (args) => (
+		<div className="flex min-h-10 flex-wrap p-11">
+			<Button size="small" spacing={{ r: 2 }}>
+				Button
+			</Button>
+			<Menu
+				{...args}
+				trigger={
+					<ButtonIcon>
+						<IconSettings />
+					</ButtonIcon>
+				}
+				spacing={{ r: 2 }}
+			>
+				<MenuItem label="Profile" icon={<IconProfile />} />
+				<MenuItem label="Statistics" icon={<IconChart />} />
+				<MenuItem label="History" icon={<IconHistory />} />
+				<MenuItem label="About" icon={<IconInfo />} />
+			</Menu>
+			<Button size="small">Button</Button>
+		</div>
+	),
+};
