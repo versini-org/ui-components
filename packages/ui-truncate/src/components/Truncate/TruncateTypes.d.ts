3b9b79f0bff44a915e5eb1b40626c346c041b728
Arno V <versini@gmail.com>
2024-12-21 14:20:43 -0500

    fix(Truncate): force word break even if there is no truncation (#777)

--- packages/ui-truncate/src/components/Truncate/TruncateTypes.d.ts
+++ packages/ui-truncate/src/components/Truncate/TruncateTypes.d.ts
@@ -5,15 +5,18 @@ export type TruncateProps = {
 	children: React.ReactNode;
 	/**
 	 * The maximum length of the text.
+	 * @default 200
 	 */
 	length?: number;
 	/**
 	 * The mode of Button. This will change the color of the Button.
+	 * @default "system"
 	 */
 	mode?: "dark" | "light" | "system" | "alt-system";
 	/**
 	 * The type of focus for the Button. This will change the color
 	 * of the focus ring around the Button.
+	 * @default "system"
 	 */
 	focusMode?: "dark" | "light" | "system" | "alt-system";
 };
