aa577658b47899829ea16574dbec8b31b70171d7
Arno V <versini@gmail.com>
2024-12-23 16:36:27 -0500

    fix(Card): using type namespace

--- packages/ui-types/ui-card-types/index.d.ts
+++ packages/ui-types/ui-card-types/index.d.ts
@@ -1,59 +1,61 @@
 import type { SpacingProps } from "@versini/ui-private/dist/utilities";
 import React from "react";
 
-export type CardProps = {
-	/**
-	 * The children to render.
-	 */
-	children: React.ReactNode;
-	/**
-	 * If the header prop is not provided, the Card must be
-	 * described via aria-labelledby.
-	 */
-	"aria-labelledby"?: string;
-	/**
-	 * CSS class(es) to add to the body.
-	 */
-	bodyClassName?: string;
-	/**
-	 * CSS class(es) to add to the main component wrapper.
-	 */
-	className?: string;
-	/**
-	 * If true, the card will be smaller.
-	 */
-	compact?: boolean;
-	/**
-	 * The content to render in the footer.
-	 */
-	footer?: React.ReactNode;
-	/**
-	 * CSS class(es) to add to the footer.
-	 */
-	footerClassName?: string;
-	/**
-	 * The content to render in the header.
-	 */
-	header?: React.ReactNode;
-	/**
-	 * CSS class(es) to add to the header.
-	 */
-	headerClassName?: string;
-	/**
-	 * The mode of Card. This will change the color of the Card.
-	 */
-	mode?: "darker" | "dark" | "light" | "system" | "alt-system";
-	/**
-	 * Whether or not to render the Card with a border.
-	 * @default false
-	 */
-	noBorder?: boolean;
-} & SpacingProps;
+declare namespace CardTypes {
+	export type Props = {
+		/**
+		 * The children to render.
+		 */
+		children: React.ReactNode;
+		/**
+		 * If the header prop is not provided, the Card must be
+		 * described via aria-labelledby.
+		 */
+		"aria-labelledby"?: string;
+		/**
+		 * CSS class(es) to add to the body.
+		 */
+		bodyClassName?: string;
+		/**
+		 * CSS class(es) to add to the main component wrapper.
+		 */
+		className?: string;
+		/**
+		 * If true, the card will be smaller.
+		 */
+		compact?: boolean;
+		/**
+		 * The content to render in the footer.
+		 */
+		footer?: React.ReactNode;
+		/**
+		 * CSS class(es) to add to the footer.
+		 */
+		footerClassName?: string;
+		/**
+		 * The content to render in the header.
+		 */
+		header?: React.ReactNode;
+		/**
+		 * CSS class(es) to add to the header.
+		 */
+		headerClassName?: string;
+		/**
+		 * The mode of Card. This will change the color of the Card.
+		 */
+		mode?: "darker" | "dark" | "light" | "system" | "alt-system";
+		/**
+		 * Whether or not to render the Card with a border.
+		 * @default false
+		 */
+		noBorder?: boolean;
+	} & SpacingProps;
 
-export type CardHeaderProps = {
-	className: string;
-	content: React.ReactNode;
+	export type HeaderProps = {
+		className: string;
+		content: React.ReactNode;
 
-	id?: string;
-	userAriaLabelledby?: string;
-};
+		id?: string;
+		userAriaLabelledby?: string;
+	};
+}
