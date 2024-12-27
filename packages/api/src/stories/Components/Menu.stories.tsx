7c5af24ccf0adcf955258205d57adcaf92e4df64
Arno V <versini@gmail.com>
2024-12-27 18:25:40 -0500

    docs: adding menu example with panel (#809)

--- packages/api/src/stories/Components/Menu.stories.tsx
+++ packages/api/src/stories/Components/Menu.stories.tsx
@@ -3,14 +3,21 @@ import type { Meta, StoryObj } from "@storybook/react";
 import "@versini/ui-menu/dist/style.css";
 import { Button, ButtonIcon } from "@versini/ui-button";
 import {
+	IconBack,
 	IconChart,
 	IconHistory,
 	IconInfo,
 	IconProfile,
 	IconSettings,
 } from "@versini/ui-icons";
+import { Panel } from "@versini/ui-panel";
 
-import { Menu, MenuItem } from "../../../../ui-menu/src/components";
+import { useState } from "react";
+import {
+	Menu,
+	MenuItem,
+	MenuSeparator,
+} from "../../../../ui-menu/src/components";
 import { commonControlsSetup } from "../../helpers/constants";
 
 type Story = StoryObj<typeof Menu>;
@@ -87,4 +94,72 @@ export const WithIcons: Story = {
 			<Button size="small">Button</Button>
 		</div>
 	),
+};
+
+export const WithMessageBox: Story = {
+	...commonControlsSetup,
+	render: (args) => {
+		const [showMessage, setShowMessage] = useState(false);
+		const handleMenuLogout = () => {
+			setShowMessage(!showMessage);
+		};
+		const onLogout = () => {
+			// console.log("==> logout!");
+			setShowMessage(!showMessage);
+		};
+		const onCancel = () => {
+			// console.log("==> cancel logout...");
+			setShowMessage(!showMessage);
+		};
+		return (
+			<>
+				<Panel
+					kind="messagebox"
+					open={showMessage}
+					onOpenChange={setShowMessage}
+					title="Log out"
+					footer={
+						<div className="flex flex-row-reverse gap-2">
+							<Button onClick={onLogout}>Log out</Button>
+							<Button mode="light" onClick={onCancel}>
+								Cancel
+							</Button>
+						</div>
+					}
+				>
+					<p>Are you sure you want to log out?</p>
+				</Panel>
+				<div className="flex min-h-10 flex-wrap p-11">
+					<Button size="small" spacing={{ r: 2 }}>
+						Button
+					</Button>
+					<Menu
+						{...args}
+						trigger={
+							<ButtonIcon>
+								<IconSettings />
+							</ButtonIcon>
+						}
+						spacing={{ r: 2 }}
+					>
+						<MenuItem label="Profile" icon={<IconProfile />} />
+						<MenuItem label="Statistics" icon={<IconChart />} />
+						<MenuItem label="History" icon={<IconHistory />} />
+						<MenuItem label="About" icon={<IconInfo />} />
+						<MenuSeparator />
+						<MenuItem
+							onClick={handleMenuLogout}
+							label="Log out"
+							icon={
+								<div className="text-red-700">
+									<IconBack monotone />
+								</div>
+							}
+						/>
+					</Menu>
+					<Button size="small">Button</Button>
+				</div>
+			</>
+		);
+	},
 };
