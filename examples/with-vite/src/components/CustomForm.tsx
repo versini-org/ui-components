48d726f5e6728e914fc6327f94d7886fcc860421
Arno V <versini@gmail.com>
2024-12-29 14:45:58 -0500

    update

--- examples/with-vite/src/components/CustomForm.tsx
+++ examples/with-vite/src/components/CustomForm.tsx
@@ -6,6 +6,7 @@ export const CustomForm = () => {
 		<form>
 			<div className="flex gap-2">
 				<TextInput
+					className="mt-4"
 					name="question"
 					label="Type your question here"
 					helperText="Powered by the sun"
