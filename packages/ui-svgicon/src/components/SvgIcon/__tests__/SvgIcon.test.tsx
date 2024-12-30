95cfe1f85b09d2d79b0e52e489cf1c4429758d57
Arno V <versini@gmail.com>
2024-12-30 14:08:10 -0500

    fix(Icons): className should combine not overwrite default class (#844)

--- packages/ui-svgicon/src/components/SvgIcon/__tests__/SvgIcon.test.tsx
+++ packages/ui-svgicon/src/components/SvgIcon/__tests__/SvgIcon.test.tsx
@@ -48,6 +48,37 @@ describe("SvgIcon prop tests", () => {
 		expect(svg.getAttribute("class")).toBe("tutu");
 	});
 
+	it("should honor the prop `defaultClassName`", async () => {
+		render(
+			<SvgIcon
+				data-testid="svgicon-1"
+				defaultViewBox="0 0 448 512"
+				defaultClassName="tutu"
+				title="toto"
+			>
+				<path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" />
+			</SvgIcon>,
+		);
+		const svg = await screen.findByTestId("svgicon-1");
+		expect(svg.getAttribute("class")).toBe("tutu");
+	});
+
+	it("should honor both props `defaultClassName` and `className`", async () => {
+		render(
+			<SvgIcon
+				data-testid="svgicon-1"
+				defaultViewBox="0 0 448 512"
+				defaultClassName="tutu"
+				className="tata"
+				title="toto"
+			>
+				<path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" />
+			</SvgIcon>,
+		);
+		const svg = await screen.findByTestId("svgicon-1");
+		expect(svg.getAttribute("class")).toBe("tutu tata");
+	});
+
 	it("should render a decorative icon by default", async () => {
 		render(
 			<SvgIcon
